ec8c4d58a0354efa452728b1eab2a27d44987093
GabrielSantosoliveira <gabrielbh@hotmail.com>
2022-08-14 17:25:12 +0000

    estilização cartas

--- magospg/src/telas/homeTela.js
+++ magospg/src/telas/homeTela.js
@@ -4,28 +4,28 @@ import { useSelecionarMago } from "../hooks/useSelecionarMago"
 
 
 export default function HomeTela() {
-    const[magos,setmago] = useRecoilState(mago)
+    const [magos, setmago] = useRecoilState(mago)
     const setMago = useSelecionarMago()
 
 
     return (
         <>
 
-            <h1> Bem-vindo al magos Pg </h1>
+            <h1 className="titulo"> Bem-vindo ao magos Pg </h1>
 
             <div className="escolhaSeuMago">
 
-                <ul>
+                <ul className="listaMagos">
 
                     {magos.map((elemento, i) => {
                         return (
                             <>
-                                <li key={elemento.id}>
+                                <li className="magos" key={elemento.id}>
 
                                     <div className="personagem" onClick={e => { setMago(elemento) }} >
-                                        <h3>{elemento.nome}</h3>
-                                        <h4>{elemento.tipo}</h4>
-                                        <img src={elemento.foto} alt="magos Imagem" />
+                                        <h3 className="nomeDoPesonagem">{elemento.nome}</h3>
+                                        <h4 className="tipoPersonagem">Tipo: {elemento.tipo}</h4>
+                                        <img className="fotoPersonagem" src={elemento.foto} alt="magos Imagem" />
                                     </div>
 
                                 </li>
@@ -38,6 +38,78 @@ export default function HomeTela() {
                 </ul>
 
             </div>
-            </>
-            )
+ <style jsx>
+                {`
+                .titulo{
+                    text-aliguscar fontes n: center;
+                    font-size:3rem;
+                    font-weight: 900;
+                }
+                .escolhaSeuMago{
+                    box-si
+                    text-aliguscar fontes n: center;
+                    font-size:3rem;
+                    font-weight: 900;
+                }
+                .escolhaSeuMago{
+                    box-sizing: border-box;
+                    padding:1;
+                    margin:0;uscar fontes 
+                    width:100vw ;
+                    
+                }
+                .listaMagos{
+                    display: flex;
+                    flex-direction: row;
+                    flex-wrap: wrap
+                    justify-content: space-around
+                    width:99%;
+                    margin: 0 auto;
+                }
+                .magos{
+                    width:90%;
+                }
+                .personagem{
+                    background-color: #0284fd ;
+                    padding:10px ;
+                    margin:10px;
+                    height: 540px ;
+                    display: flex;
+                    flex-direction: column;
+                    justify-content:space-around ;
+                    border:10px solid rgb(220, 245, 1);
+                    border-radius: 10px; 
+                    opacity:0.9;
+                    transition: all 500ms ;
+                    box-shadow: 2px 2px 3px 2px rgba(0, 0, 0, 0.39);
+                }
+                .personagem:hover{
+                    
+
+                    background-color: #1a90fd ;
+                    opacity:1;
+                    transform: scale(1.07) ;
+                    box-shadow: 12px 13px 11px 2px rgba(0, 0, 0, 0.568);
+
+
+                }
+                .nomeDoPesonagem{
+                    font-size: 2.5rem ;
+                    text-align: center;
+                    font-weight:900 ;
+                }
+                .tipoPersonagem{
+                    font-size:1.5rem;
+                    font-weight:900;
+                }
+                .fotoPersonagem{
+                    width:99%;
+                    height:300px;
+                    margin:5px;
+
+                }
+                `}
+            </style>
+        </>
+    )
 }
--- magospg/src/telas/lutaTela.js
+++ magospg/src/telas/lutaTela.js
@@ -15,28 +15,84 @@ export default function LutaTela() {
         <>
             <h1> começe a luta </h1>
 
-            <div>
+            <div className="personagem">
 
-                <h2> seu mago </h2>
+                <h2 className="personagemTitulo"> seu mago </h2>
 
-                <img src={mago.foto} />
-                <h3> {mago.nome}</h3>
-                <h4>{mago.tipo}</h4>
-                <h4>{mago.vida}</h4>
+                <img  className="personagemImg" src={mago.foto} />
+                <h3 className="personagemNome"> {mago.nome}</h3>
+                <h4 className="personagemTipo">{mago.tipo}</h4>
+                <h4 className="personagemVida">{mago.vida}</h4>
             </div>
             <button onClick={jogada}> Jogada </button>
 
-            <div>
-                <img src={monstroAtom.foto} />
-                <h2> Monstro </h2>
-                <h4>{monstroAtom.descricao}</h4>
-                <h4>{monstroAtom.status}</h4>
-                <h3> {monstroAtom.nome}</h3>
-                <h4>{monstroAtom.vida}</h4>
+            <div className="personagem">
+                <h2 className="personagemTitulo" > Monstro </h2>
+                <img className="personagemImg" src={monstroAtom.foto} />
+                <h3 className="personagemNome" > {monstroAtom.nome}</h3>
+                <h4 className="persornagemDescrisão">{monstroAtom.descricao}</h4>
+                <h4 className="persornagemStatus">{monstroAtom.status}</h4>
+                <h4 className="personagemVida">{monstroAtom.vida}</h4>
             </div>
 
             <button onClick={sortearNumero}> rodar dados</button>
+        <style jsx>
+            {`
+            
+            
+            .personagem{
+                box-sizing: border-box;
+                background-color:#02529c;
+                width:48% ;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                border: 10px solid rgb(236, 255, 65);
+                border-radius:5px;
+            }
 
+            .personagemTitulo{
+                font-size: 2rem;
+                font-weight: 900;
+                margin-bottom: 10px;
+
+            }
+
+            .personagemImg{
+                width: 90%;
+            }
+            .personagemNome{
+                font-size: 2rem;
+                margin-bottom: 10px;
+            }
+
+            .personagemTipo{
+                font-size: 1.8rem;
+                color: rgba(235, 193, 193, 0.863);
+                margin-bottom: 10px;
+            }
+
+            .personagemVida{
+                font-size: 2.5rem;
+                font-weight: 900;
+                color: rgb(253, 29, 29);
+            }
+            .persornagemDescrisão{
+                font-size: 1.6rem;
+                text-align: justify;
+                padding: 1rem ;
+                margin: 1rem;
+
+            }
+            .persornagemStatus{
+                font-size: 1.9rem;
+                text-align: center;
+                padding: 1rem ;
+                margin: 1rem;
+            }
+            `}
+        </style>
         </>
     )
 }
